a9d86cac267a9786e64eafc8cba8c7d11c9276fb
rizal-nugraha <rizal.nugrah@gmail.com>
2019-12-03 22:33:16 +0700

    ganti

--- application/views/home.php
+++ application/views/home.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Simple Crud Codeigniter</title>
+    <title>coba crud</title>
 
     <!-- Bootstrap -->
     <link rel="stylesheet" href="<?=base_url()?>assets/css/bootstrap.min.css">
